ef89e72ac0d9289fa8bf36a31a3d5240bff6c13a
郑付超 <fuchao.zheng@ikang.com>
2017-05-21 15:22:35 +0800

    use mint-ui2.0

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(MintUI)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
 })
